1b9a652e473fef1b406d16cd299b994556314be6
kamiyagordon <bigoswan1@gmail.com>
2026-03-02 10:51:34 +0900

    Add files via upload

--- PalmerPenguinsM7.java
+++ PalmerPenguinsM7.java
@@ -1,15 +1,15 @@
 // PalmerPenguinsM7.java
-//
-//
+// Sakura Gordon
+// 3/1/2026
 // Refactored Palmer Penguins analysis using object-oriented approach
 
 public class PalmerPenguinsM7 {
     public static void main(String[] args) {
         // Create an instance of SpeciesCounter
-        
+        SpeciesCounter counter = new SpeciesCounter();
 
         // Read species data
-        
+        counter.readSpeciesData();
 
         // Initialize species count
         counter.initializeSpeciesCount();
